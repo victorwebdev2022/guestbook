3cfbf6b9626e193fc8cf3e0b4f6ba762de5477d1
victorwebdev2022 <victorguerra60200@gmail.com>
2022-11-21 13:42:04 +0100

    commit

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -11,9 +11,7 @@ class ConferenceController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+       
         
         
         return new Response(<<<EOF
