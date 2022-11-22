e95e285f096ffdff58ed06d14f99fe83ea405aff
victorwebdev2022 <victorguerra60200@gmail.com>
2022-11-22 16:56:02 +0100

    commit

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
@@ -46,7 +47,8 @@ class CommentCrudController extends AbstractCrudController
          yield TextareaField::new('text')
              ->hideOnIndex()
          ;
-         yield TextField::new('photoFilename')
+         yield ImageField::new('photoFilename')
+            ->setBasePath('/uploads/photos')            ->setLabel('Photo')
             ->onlyOnIndex()
          ;
  
